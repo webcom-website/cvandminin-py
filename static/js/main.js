868fe30e243e5abe5c081f3aeb564455c8531357
webcom-website <webcom.website@gmail.com>
2021-09-18 14:35:43 -0300

    add cv python

---
+++ static/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+    if (location.pathname.indexOf('debug') > -1 ){
+               //cambio 
+        $('#view-on-codepen').css('display','inline-table')
+    }
+});
